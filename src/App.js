b1ce98cf9d0ca15f8dd32ece304b97e5f0a1c370
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-08-01 11:07:14 +0100

    Make sure I install the neccessary dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/homePage/HomePage';
+import MyAppointments from './components/appointmentInfo/MyAppointments';
+import DoctorList from './components/doctorInfo/DoctorList';
+import DeleteDoctor from './components/doctorInfo/DeleteDoctor';
+import PatientList from './components/patientInfo/PatientList';
+import Appointment from './components/appointmentInfo/Appointment';
+
 function App() {
   return (
-    <div className="App">
-      <h1> Hello doctor appoint front-end app</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/doctors" element={<DoctorList />} />
+        <Route path="/delete_doctor" element={<DeleteDoctor />} />
+        <Route path="/patients" element={<PatientList />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/my_appointments" element={<MyAppointments />} />
+      </Routes>
+    </Router>
   );
 }
 
