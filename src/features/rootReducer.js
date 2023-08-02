e1fb77f46cf18cddc89d0ee5ce9913b83facfd15
rails --v <ruby -v>
2023-08-02 18:21:51 +0300

    Update rootReducer.js file

--- src/features/rootReducer.js
+++ src/features/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import authSlice from './authSlice';
+import appointmentSlice from './appointmentSlice';
+import doctorSlice from './doctorSlice';
 
 const rootReducer = combineReducers({
   auth: authSlice,
+  appointments: appointmentSlice,
+  doctors: doctorSlice,
 });
 
 export default rootReducer;
