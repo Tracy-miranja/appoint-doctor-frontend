1f9f6e17065ad0bb98b9150139e3db728b3681c9
danutibe07 <danielutibe07@gmail.com>
2023-08-08 10:44:27 +0100

    fetched data & indexed each item with useSelector

--- src/components/doctorInfo/DeleteDoctor.js
+++ src/components/doctorInfo/DeleteDoctor.js
@@ -7,6 +7,26 @@ import NavBar from '../navbar/Navbar';
 import { fetchDoctors, deleteDoctor } from '../../features/doctorSlice';
 
 function DeleteDoctor() {
+  const dispatch = useDispatch();
+  const {
+    status, error, doctors, references,
+  } = useSelector((state) => state.doctors);
+  const totalCount = references ? references
+    .reduce((count, appointments) => count + appointments.length, 0) : 0;
+  useEffect(() => {
+    dispatch(fetchDoctors());
+  }, [dispatch]);
+
+  if (status === 'loading') {
+    return (
+      <div>
+        <NavBar />
+        <h1>
+          loading....
+        </h1>
+      </div>
+    );
+  }
   return (
     <div>
       <h2>Delete Doctor Page</h2>
