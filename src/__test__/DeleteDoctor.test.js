f3a6c0248e0d9b8e31b8dde883da8f82373f10e8
danutibe07 <danielutibe07@gmail.com>
2023-08-10 11:43:18 +0100

    Added tests for Doctor List

--- src/__test__/DeleteDoctor.test.js
+++ src/__test__/DeleteDoctor.test.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../features/store';
 import DeleteDoctor from '../components/doctorInfo/DeleteDoctor';
-​
+
 it('renders correctly', () => {
   const tree = renderer
     .create(
