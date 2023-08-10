2c4df33fb7698719798e8cb212bdbd80ffb8424a
Tracy-miranja <barazatracy44@gmail.com>
2023-08-10 10:17:25 +0300

    Refactor patientlist file

--- src/components/forSuperUser/PatientList.js
+++ src/components/forSuperUser/PatientList.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import NavBar from '../navbar/Navbar';
 
-function PatientList() {
-  return (
-    <div>
-      <NavBar />
-      <h2>Patients List</h2>
-      {/* Display a list of patients here */}
-    </div>
-  );
-}
+const PatientList = () => (
+  <div>
+    <NavBar />
+    <h2>Patients List</h2>
+    {/* Display a list of patients here */}
+  </div>
+);
 
 export default PatientList;
