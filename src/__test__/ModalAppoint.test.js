329f64f4cbbfa0d6ffc71f3c0dd2c1194c68f39e
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-08-10 12:20:49 +0100

    Add testing for ModalAppoint function

--- src/__test__/ModalAppoint.test.js
+++ src/__test__/ModalAppoint.test.js
@@ -2,14 +2,14 @@ import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../features/store';
-import AppointmentList from '../components/forSuperUser/AppointmentList';
+import ModalAppoint from '../components/appointmentInfo/ModalAppoint';
 
 it('renders correctly', () => {
   const tree = renderer
     .create(
       <Provider store={store}>
         <Router>
-          <AppointmentList />
+          <ModalAppoint />
         </Router>
       </Provider>,
     )
